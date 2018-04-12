1cd56379337ce42087f7c537b0abf15b23751e99
Amic016 <36950611+Amic016@users.noreply.github.com>
2018-04-12 18:02:18 +0900

    Update SqlExecute.bat

--- sandbox/SqlExecute.bat
+++ sandbox/SqlExecute.bat
@@ -2,5 +2,5 @@ del log.txt
 set PGPASSWORD=password
 set PGCLIENTENCODING=utf8
 for /f %%a in ('dir *.sql /b /on') do (
-	psql -d ds1 -U username -f %%a 2>&1
+	psql -d databasename -U username -f %%a 2>&1
 ) >> log.txt
